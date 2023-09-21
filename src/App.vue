d34e322452ec02351393668c2cbba8fb1c9cfe59
plechoss <plaskacz.panda@gmail.com>
2023-09-21 15:17:41 +0200

    First working version of the photos tab

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import router from './router'
 import { useDisplay } from 'vuetify'
-const { name } = useDisplay()
+
+const { xs, sm } = useDisplay()
 
 const navDrawer = ref(false)
 const links = ref([
@@ -19,39 +20,57 @@ function goTo(path: string) {
   router.push(path)
 }
 
-const breakpoint = computed(() => {
-  return name.value
-})
 </script>
 
 <template>
   <v-app>
-    <v-main> </v-main>
+    <v-app-bar density="prominent" elevation="0" fluid>
+      <v-row align="center" justify="center" class="fill-height mt-0">
+        <v-spacer />
+        <v-col v-if="!(sm || xs)">
+          <span
+            @click="goTo('/photos')"
+            class="text-button font-weight-bold text-grey-darken-2 nav-link"
+            >PHOTOS</span
+          >
+        </v-col>
+        <v-col>
+          <span @click="goTo('/photos')" class="text-h5 nav-link"> PLESKOWICZ </span>
+        </v-col>
+        <v-col v-if="!(sm || xs)">
+          <span
+            @click="goTo('/about')"
+            class="text-button text-grey-darken-2 font-weight-bold nav-link"
+            >ABOUT</span
+          >
+        </v-col>
+        <v-spacer />
+      </v-row>
+      <template #append>
+        <v-app-bar-nav-icon v-if="xs || sm" @click="onNavigationDrawerButtonClick"></v-app-bar-nav-icon>
+      </template>
+    </v-app-bar>
+    <v-main> <router-view> </router-view> </v-main>
+    <v-navigation-drawer v-model="navDrawer" location="right" :disable-resize-watcher="true" floating>
+      <v-list-item v-for="link in links" :key="link.name">
+        <span @click="goTo(link.to)" class="text-button nav-link"> {{ link.name }} </span>
+      </v-list-item>
+    </v-navigation-drawer>
   </v-app>
-  <v-app-bar density="compact">
-    <v-spacer />
-    <span @click="goTo('/photos')" class="text-button font-weight-bold text-grey-darken-2 nav-link"
-      >PHOTOS</span
-    >
-    <v-toolbar-title class="flex text-center">
-      <v-span @click="goTo('/photos')" class="text-h5 nav-link"> MICHAŁ PLESKOWICZ </v-span>
-    </v-toolbar-title>
-    <span @click="goTo('/about')" class="text-button text-grey-darken-2 font-weight-bold nav-link"
-      >ABOUT</span
-    >
-    <v-spacer />
-    <template #append>
-      <v-app-bar-nav-icon dark @click="onNavigationDrawerButtonClick"></v-app-bar-nav-icon>
-    </template>
-  </v-app-bar>
-  <router-view></router-view>
 </template>
 
-<style scoped>
+<style>
 .nav-link:hover {
   cursor: pointer;
 }
-img {
-  width: 100%;
+.v-main {
+  width: 100vw !important;
+}
+#app {
+  margin: 0px !important;
+  padding: 0px !important;
+}
+.v-toolbar__content {
+  text-align: center !important;
 }
 </style>
--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,93 +1,46 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { xs, sm, md } = useDisplay()
+
+const numPhotos = ref(48)
+
+const cols = computed(() => {
+  if (md.value) return 3
+  if (sm.value) return 4
+  if (xs.value) return 12
+  else return 2
+})
+
+const numPhotosPerCol = computed(() => {
+  return Math.floor(numPhotos.value * cols.value / 12)
+})
 </script>
 
 <template>
-  <!-- <v-container fluid>
-      </v-container> -->
-  <v-container fluid>
-    <v-row class="d-flex flex-row">
-      <v-col class="d-flex child-flex">
-        <span class="text-h3"> Tytuł strony </span>
-      </v-col>
-    </v-row>
-    <v-row class="d-flex flex-row">
-      <v-col class="d-flex child-flex">
-        <span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum
-        </span>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-img :src="`../src/assets/photos/0001.jpg`"></v-img>
-      </v-col>
-    </v-row>
-    <!-- <v-row>
-          <v-col
-            v-for="i in Array(10)
-              .fill(10)
-              .map((x, y) => x + y)"
-            :key="i"
-            class="d-flex child-flex"
-          >
-            <v-img :src="`../src/assets/photos/00${i}.jpg`"></v-img>
-          </v-col>
-          <v-col
-            v-for="i in Array(10)
-              .fill(20)
-              .map((x, y) => x + y)"
-            :key="i"
-            class="d-flex child-flex"
-          >
-            <v-img :src="`../src/assets/photos/00${i}.jpg`"></v-img>
-          </v-col>
-          <v-col
-            v-for="i in Array(30)
-              .fill(10)
-              .map((x, y) => x + y)"
-            :key="i"
-            class="d-flex child-flex"
-          >
-            <v-img :src="`../src/assets/photos/00${i}.jpg`"></v-img>
-          </v-col>
-          <v-col
-            v-for="i in Array(10)
-              .fill(40)
-              .map((x, y) => x + y)"
-            :key="i"
-            class="d-flex child-flex"
-          >
-            <v-img :src="`../src/assets/photos/00${i}.jpg`"></v-img>
-          </v-col>
-        </v-row> -->
-    <v-row class="d-flex child-flex">
-      <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
-        <v-img
-          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-          aspect-ratio="1"
-          cover
-          class="bg-grey-lighten-2"
+  <v-container fluid class="pt-0 mt-0">
+    <v-row class="pt-0 mt-0">
+      <v-col v-for="colIdx in Array(12 / cols).keys()" :key="`col${colIdx}`" class="ma-0 pa-0" :cols="cols">
+        <v-row
+          v-for="i in Array(numPhotosPerCol)
+            .fill(colIdx * numPhotosPerCol + 1)
+            .map((x, y) => x + y)"
+          :key="i"
+          no-gutters
         >
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
+          <v-col>
+            <v-lazy
+              :min-height="100"
+              :options="{ threshold: 0.5 }"
+              transition="fade-transition"
+              class="pr-1 pb-1 pt-0 pl-0"
+            >
+              <v-img :src="`../src/assets/photos/${i.toString().padStart(4, '0')}.jpg`"></v-img>
+            </v-lazy>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
-  <!--     
-    <v-navigation-drawer v-model="navDrawer" location="right" sticky>
-      <v-list-item v-for="link in links" :key="link.name">
-        {{ link.name }}
-      </v-list-item>
-    </v-navigation-drawer> -->
 </template>
